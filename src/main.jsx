d17334a0014ba30f19f95745a14b836b1c143ccc
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-04 09:13:29 +0000

    fixed reference in main.jsx to education component
    reference was using incorect path and capitalisation

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import GeneralInfo from './components/General_info.jsx'
-import {EducationSection} from './components/education.jsx'
+import {EducationSection} from './src/components/Education.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

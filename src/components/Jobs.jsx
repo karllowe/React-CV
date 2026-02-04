21439cb23a43194d457173d5feada89b0743a104
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-04 21:11:57 +0000

    added ability to edit/ delete jobs

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -18,11 +18,27 @@ class Job {
 function JobsSection() {
     const [jobsArray, setJobsArray] = useState([]);
     const [showJobsModal, setJobsModal] = useState(false);
+    const [selectedJob, setSelectedJob] = useState(null);
    
     const addNewJob = (title, company, startDate, endDate=null, responsibilities="") => {
         const newJob = new Job(title, company, startDate, endDate, responsibilities);
         setJobsArray((prev) => [...prev, newJob])
     }
+
+    const updateJob = (newTitle, newCompany, newStartDate, newEndDate=null, newResponsibilities="", existingId) => {
+        const index = jobsArray.findIndex((item) => item.id === existingId);
+        const updatedJob = {title:newTitle, company:newCompany, startDate:newStartDate, endDate:newEndDate, responsibilities:newResponsibilities, id:existingId};
+        jobsArray.splice(index, 1, updatedJob);
+    };
+
+    const deleteJob = (deleteId) => {
+        const index = jobsArray.findIndex((item) => item.id === deleteId);
+        jobsArray.splice(index, 1)
+    };
+
+    const setFormContents = (selectedJob) => {
+        setSelectedJob(selectedJob)
+    };
 
     const tempJobsArray = jobsArray;
     tempJobsArray.sort((a, b) => sortDates(a.endDate, b.endDate));
@@ -34,29 +50,40 @@ function JobsSection() {
                 <button
                     type="button"
                     onClick={() => {
+                        setSelectedJob(null);
                         setJobsModal((v) => !v);
                     }}
                 >
                     Add
                 </button>
             </div>
-            <AddJob jobsArray={jobsArray} setJobsModal={setJobsModal} showJobsModal={showJobsModal} addNewJobs={addNewJob}/>
-            <ShowJobs jobsArray={tempJobsArray}/>
+            {showJobsModal && (<AddJob jobsArray={jobsArray} setJobsModal={setJobsModal} 
+            showJobsModal={showJobsModal} addNewJobs={addNewJob} selectedJob={selectedJob} 
+            updateJob={updateJob} deleteJob={deleteJob}/>)}
+            <ShowJobs jobsArray={tempJobsArray} setJobsModal={setJobsModal} setFormContents={setFormContents}/>
         </div>
     )
 }
 
-function AddJob({showJobsModal, setJobsModal, addNewJobs}) {
+function AddJob({showJobsModal, setJobsModal, addNewJobs, selectedJob, updateJob, deleteJob}) {
     const dialogRef = useRef(null);
     const formRef = useRef(null);
 
-    const [title, setTitle] = useState("");
-    const [company, setCompany] = useState("");
-    const [startDate, setStartDate] = useState("");
-    const [endDate, setEndDate] = useState("");
-    const [responsibilities, setResponsibilities] = useState("");
+    const [title, setTitle] = useState(selectedJob ? selectedJob.title : "");
+    const [company, setCompany] = useState(selectedJob ? selectedJob.company : "");
+    const [startDate, setStartDate] = useState(selectedJob ? selectedJob.startDate : "");
+    const [endDate, setEndDate] = useState(selectedJob ? selectedJob.endDate : "");
+    const [responsibilities, setResponsibilities] = useState(selectedJob ? selectedJob.responsibilities : "");
+    const [id] = useState(selectedJob ? selectedJob.id : "");
 
     const [isValid, setIsValid] = useState(false);
+
+    let mode = "";
+    if (selectedJob) {
+        mode = "edit"
+    } else {
+        mode = "new"
+    };
 
 
     useEffect(() => {
@@ -94,17 +121,34 @@ function AddJob({showJobsModal, setJobsModal, addNewJobs}) {
         >
             <div className="header">
                 <h2>Add new job</h2>
+                <button
+                    type="button"
+                    className="deleteButton"
+                    hidden={mode=="new"}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        deleteJob(id);
+                        resetForm();
+                        setJobsModal(false);
+                    }}
+                >
+                    Delete
+                </button>
                 <button 
                     type="submit"
                     className="submitButton"
                     disabled={!isValid}
                     onClick={(e) => {
                         e.preventDefault();
-                        addNewJobs(title, company, startDate, endDate, responsibilities);
+                        if (mode=="edit") {
+                            updateJob(title, company, startDate, endDate, responsibilities);
+                        } else {
+                            addNewJobs(title, company, startDate, endDate, responsibilities);
+                        };
                         resetForm();
                         setJobsModal(false)
                     }}>
-                    Add new
+                    Save
                 </button>
                 <button type="button" onClick={() => setJobsModal(false)}>Close</button>
             </div>
@@ -167,7 +211,7 @@ function AddJob({showJobsModal, setJobsModal, addNewJobs}) {
     )
 }
 
-function ShowJobs({jobsArray}) {
+function ShowJobs({jobsArray, setJobsModal, setFormContents}) {
     return (
         <>
         <p>{jobsArray.length <1 ? "No jobs added" : ""}</p>
@@ -178,6 +222,15 @@ function ShowJobs({jobsArray}) {
                     <div className="jobListItem">
                         <div>{item.title} - {item.company}</div>
                         <div className="jobDate">({format(item.startDate, 'MMM-yyyy')}-{item.endDate ? format(item.endDate, 'MMM-yyyy') : "current"}) </div>
+                        <button
+                            type="button"
+                            onClick={() => {
+                                setFormContents(item);
+                                setJobsModal(true)
+                            }}
+                        >
+                            Edit
+                        </button>
                     </div>
                 </li>
             ))
